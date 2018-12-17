1f8f583d70d7af7031ba7ec62d13b93381848ffa
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 04:46:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length)
+  return array.slice(0, array.length - 1)
 }
 
 function accessElementInArray(array , index){
